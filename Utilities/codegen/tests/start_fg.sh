ed82ce624dfdf87cdb900c3cd98afc734fc232d7
Pavel Vainerman <pv@etersoft.ru>
2009-03-11 00:46:25 +0300

    first version for new codegen. Use "node" from xxx.src.xml

---
+++ Utilities/codegen/tests/start_fg.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ulimit -Sc 1000000
+
+uniset-start.sh -f ./test --name TestProc --confile test.xml --unideb-add-levels info,crit,warn,level1,level8 \
+--sm-ready-timeout 5000
+#info,warn,crit,system,level9 > 1.log
+#--c-filter-field cfilter --c-filter-value test1 --s-filter-field io --s-filter-value 1
+
