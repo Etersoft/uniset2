bdb576d660b079c1352b8425170494cc8e0fa9e9
Vitaly Lipatov <lav@etersoft.ru>
2009-08-05 19:50:54 +0400

    add some tests

---
+++ tests/getline.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	cout << "size of str=" << str.size() << ", strlen=" << strlen(str.c_str()) << endl;
+}
