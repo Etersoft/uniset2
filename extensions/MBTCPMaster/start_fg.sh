5a822b8e6fc6e439995825c33a3e0ce3a1ed4650
Pavel Vaynerman <pv@etersoft.ru>
2009-09-09 01:44:20 +0400

    ??

--- extensions/MBTCPMaster/start_fg.sh
+++ extensions/MBTCPMaster/start_fg.sh
@@ -4,7 +4,6 @@ uniset-start.sh -f ./uniset-mbtcpmaster --mbtcp-name MBTCPExchange1 \
 --dlog-add-levels info,crit,warn \
 --mbtcp-filter-field CAN2sender \
 --mbtcp-filter-value SYSTSNode \
---mbtcp-reg-from-id 1 \
 --mbtcp-gateway-iaddr 127.0.0.1 \
 --mbtcp-gateway-port 2048
 #--mbtcp-filter-field mbtcp --mbtcp-filter-value 1
