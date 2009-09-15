3706b6d35611d377e18b5474b0c1dd007cfca0fc
Vitaly Lipatov <lav@etersoft.ru>
2009-09-15 20:55:05 +0400

    fix logFile

--- extensions/MBTCPMaster/MBTCPMaster.cc
+++ extensions/MBTCPMaster/MBTCPMaster.cc
@@ -669,14 +669,14 @@ void MBTCPMaster::sysCommand( UniSetTypes::SystemMessage *sm )
 			string fname = unideb.getLogFile();
 			if( !fname.empty() )
 			{
-				unideb.logFile(fname.c_str());
+				unideb.logFile(fname);
 				unideb << myname << "(sysCommand): ***************** UNIDEB LOG ROTATE *****************" << std::endl;
 			}
 			dlog << myname << "(sysCommand): logRotate" << std::endl;
 			fname = dlog.getLogFile();
 			if( !fname.empty() )
 			{
-				dlog.logFile(fname.c_str());
+				dlog.logFile(fname);
 				dlog << myname << "(sysCommand): ***************** dlog LOG ROTATE *****************" << std::endl;
 			}
 		}
