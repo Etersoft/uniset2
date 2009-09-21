3484f739d7cd559d41ead89cee424148b10863cb
Pavel Vaynerman <pv@etersoft.ru>
2009-09-22 02:09:54 +0400

    debugging...

--- extensions/RTUExchange/start_fg.sh
+++ extensions/RTUExchange/start_fg.sh
@@ -6,7 +6,7 @@ uniset-start.sh -f ./uniset-rtuexchange --confile test.xml \
 	--rs-speed 38400 \
 	--rs-filter-field rs \
 	--rs-filter-value 3 \
-	--dlog-add-levels crit,warn \
+	--dlog-add-levels info,crit,warn \
 	--rs-force 0 \
 	--rs-force-out 0 \
 #,level3
