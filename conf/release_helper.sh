a45e410a0c4b0bbdc2f76e70dc9472d777463dd8
Vitaly Lipatov <lav@etersoft.ru>
2009-09-29 14:32:28 +0000

    updated release_helper

--- conf/release_helper.sh
+++ conf/release_helper.sh
@@ -1,23 +1,44 @@
 #!/bin/sh
 # Вспомогательный скрипт для подготовки и сборки rpm-пакета с системой
-. /etc/rpm/etersoft-build-functions
 
-cd ..
-SPECNAME=conf/libuniset.spec
-RPMDIR=~/RPM/RPMS/$DEFAULTARCH
-RPMSOURCEDIR=~/RPM/SOURCES
-FTPDIR=/var/ftp/pub/Ourside/i586/RPMS.uniset
-PROJECT=uniset
-GEN=/var/ftp/pub/Ourside/i586/genb.sh
+ETERBUILDVERSION=163
+. /usr/share/eterbuild/eterbuild
+load_mod spec
+
+REL=eter
+MAILDOMAIN=server
 BACKUPDIR=$FTPDIR/backup
+RPMBINDIR=$RPMDIR/RPMS
+# builder50 path
+TOPDIR=/var/ftp/pvt/Etersoft/Ourside/
+
+PROJECT=$1
+test -n "$PROJECT" || PROJECT=uniset
+
+SPECNAME=libuniset.spec
+
+FTPDIR=$TOPDIR/i586/RPMS.$PROJECT
+if [ -d "$FTPDIR" ] ; then
+	GEN="genbasedir --create --progress --topdir=$TOPDIR i586 $PROJECT"
+else
+	# For NoteBook build
+	FTPDIR=/var/ftp/pub/Ourside/i586/RPMS.$PROJECT
+	GEN=/var/ftp/pub/Ourside/i586/genb.sh
+fi
+
+fatal()
+{
+	echo "Error: $@"
+	exit 1
+}
 
 function send_notify()
 {
-	export EMAIL="$USER@server"
+	export EMAIL="$USER@$MAILDOMAIN"
 	CURDATE=`date`
-	MAILTO="devel@server"
+	MAILTO="devel@$MAILDOMAIN"
 # FIXME: проверка отправки
-mutt $MAILTO -s "[uniset] New build: $BUILDNAME" <<EOF
+mutt $MAILTO -s "[$PROJECT] New build: $BUILDNAME" <<EOF
 Готова новая сборка: $BUILDNAME
 -- 
 your $0
@@ -26,51 +47,19 @@ EOF
 echo "inform mail sent to $MAILTO"
 }
 
-prepare_tarball()
-{
-	build_rpms_name $SPECNAME
-
-	NAMEVER=$BASENAME-$VERSION
-	WDPROJECT=$(pwd)
-	TARNAME=$NAMEVER.tar
-	DESTDIR=$TMPDIR/$NAMEVER
-	RET=0
-
-	mkdir -p $DESTDIR
-	rm -rf $DESTDIR/*
-	cp -r $WDPROJECT/* $DESTDIR/
-	cd 	$DESTDIR/
-		make distclean
-		[ -a ./autogen.sh ] && ./autogen.sh
-		rm -rf autom4te.cache/
-		rm -rf .git
-		rm -rf .gear
-	
-		echo "Make tarball $TARNAME ... from $DESTDIR"
-		mkdir -p $RPMSOURCEDIR/
-		$NICE tar cf $RPMSOURCEDIR/$TARNAME ../$NAMEVER $ETERTARPARAM || RET=1
-		rm -rf $DESTDIR
-	cd -
-
-	[ $RET ] && echo "build ok" || fatal "Can't create tarball"
-}
-
 function cp2ftp()
 {
 	mkdir -p $BACKUPDIR
 	mv -f $FTPDIR/*$PROJECT* $BACKUPDIR/
-	mv -f $RPMDIR/*$PROJECT* $FTPDIR/
+	mv -f $RPMBINDIR/*$PROJECT* $FTPDIR/
 	chmod 'a+rw' $FTPDIR/*$PROJECT*
 	$GEN
 }
 
+
 # ------------------------------------------------------------------------
-build_rpms_name $SPECNAME
-export BUILDNAME=$BASENAME-$VERSION-$RELEASE
 
 add_changelog_helper "- new build" $SPECNAME
-
-prepare_tarball || fatal "Can't prepare tarball"
 
 rpmbb $SPECNAME || fatal "Can't build"
 
@@ -79,7 +68,7 @@ cp2ftp
 #rpmbs $SPECNAME
 #send_notify
 
-# Увеличиваем релиз
+# Увеличиваем релиз и запоминаем спек после успешной сборки
 inc_release $SPECNAME
 # и запоминаем спек после успешной сборки
 #cvs commit -m "Auto updated by $0 for $BUILDNAME" $SPECNAME || fatal "Can't commit spec"
