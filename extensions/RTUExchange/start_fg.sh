4974e60b946b70b5985bcf031c7bc5e39a90b237
Pavel Vaynerman <pv@etersoft.ru>
2010-03-28 15:34:31 +0400

    modify for current configuration

--- extensions/RTUExchange/start_fg.sh
+++ extensions/RTUExchange/start_fg.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 uniset-start.sh -f ./uniset-rtuexchange --confile test.xml \
+	--smemory-id SharedMemory \
 	--rs-dev /dev/cbsideA0 \
-	--rs-name RSExchange \
+	--rs-name RTUExchange \
 	--rs-speed 38400 \
 	--rs-filter-field rs \
-	--rs-filter-value 3 \
+	--rs-filter-value 1 \
 	--dlog-add-levels info,crit,warn \
 	--rs-force 0 \
 	--rs-force-out 0 \
