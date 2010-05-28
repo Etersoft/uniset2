99aa959a7d0792de1c52aeeae5cf83c282f57aee
Vitaly Lipatov <lav@etersoft.ru>
2010-05-29 02:51:32 +0400

    add compile flags, always run configure

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,10 @@
 
 autoreconf -fiv
 
-# run configure if project is compiled
-test -f Makefile && ./configure --enable-maintainer-mode --prefix=/usr
-exit 0
+# set flags as in rpm build
+CFLAGS='-pipe -O2 -march=i586 -mtune=i686'
+export CFLAGS
+CXXFLAGS="$CFLAGS"
+export CXXFLAGS
+
+./configure --enable-maintainer-mode --prefix=/usr
