33661a2f6a6281cc0aa6798ea38097511c2d4eea
Pavel Vaynerman <pv@etersoft.ru>
2010-12-14 03:15:19 +0300

    UDP: сделал пакет постоянной длины..

--- extensions/UDPExchange/start_fg2.sh
+++ extensions/UDPExchange/start_fg2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-uniset-start.sh -f ./uniset-udpexchange --udp-name UDPExchange2 --udp-host localhost --udp-port 2049 \
+uniset-start.sh -f ./uniset-udpexchange --udp-name UDPExchange2 --udp-host localhost --udp-port 3001 \
 	--confile test.xml \
-	--udp-filter-field udp --udp-filter-value 2 \
+	--udp-filter-field udp --udp-filter-value 2we \
 	--udp-ip \
 	--dlog-add-levels info,crit,warn
 
