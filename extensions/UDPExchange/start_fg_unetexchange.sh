44925d72f3f81eb82545076ceff63b8ec9d45c5c
Pavel Vaynerman <pv@etersoft.ru>
2010-12-22 02:33:00 +0300

    UNet2: Отладил механизм защиты от сбоя счётчика пакетов.

--- extensions/UDPExchange/start_fg_unetexchange.sh
+++ extensions/UDPExchange/start_fg_unetexchange.sh
@@ -2,6 +2,6 @@
 
 uniset-start.sh -f ./uniset-unetexchange --unet-name UNetExchange \
 	--confile test.xml \
-	--unet-filter-field rs --unet-filter-value 2 \
+	--unet-filter-field rs --unet-filter-value 2 --unet-maxdifferense 40 \
 	--dlog-add-levels info,crit,warn
 
