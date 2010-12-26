99cb9d26de24be37772b5de711317f1d02da40a5
Pavel Vaynerman <pv@etersoft.ru>
2010-12-27 00:38:16 +0300

    Забыл добавить UDPPacket

---
+++ extensions/UNet2/UDPPacket.cc
@@ -0,0 +1,45 @@
+#include "UDPPacket.h"
+// -----------------------------------------------------------------------------
+using namespace std;
+using namespace UniSetUDP;
+// -----------------------------------------------------------------------------
+std::ostream& UniSetUDP::operator<<( std::ostream& os, UniSetUDP::UDPHeader& p )
+{
+	return os << "nodeID=" << p.nodeID 
+				<< " procID=" << p.procID 
+				<< " dcount=" << p.dcount
+				<< " pnum=" << p.num;
+}
+// -----------------------------------------------------------------------------
+std::ostream& UniSetUDP::operator<<( std::ostream& os, UniSetUDP::UDPData& p )
+{
+	return os << "id=" << p.id << " val=" << p.val;
+}
+// -----------------------------------------------------------------------------
+std::ostream& UniSetUDP::operator<<( std::ostream& os, UniSetUDP::UDPMessage& p )
+{
+	return os;
+}
+// -----------------------------------------------------------------------------
+UDPMessage::UDPMessage():
+count(0)
+{
+}
+// -----------------------------------------------------------------------------
+bool UDPMessage::addData( const UniSetUDP::UDPData& dat )
+{
+	if( count >= MaxDataCount )
+		return false;
+
+	msg.dat[count] = dat;
+	count++;
+	msg.header.dcount = count;
+	return true;
+}
+// -----------------------------------------------------------------------------
+bool UDPMessage::addData( long id, long val)
+{
+	UDPData d(id,val);
+	return addData(d);
+}
+// -----------------------------------------------------------------------------
---
+++ extensions/UNet2/UDPPacket.h
@@ -0,0 +1,69 @@
+// $Id: UDPPacket.h,v 1.1 2009/02/10 20:38:27 vpashka Exp $
+// -----------------------------------------------------------------------------
+#ifndef UDPPacket_H_
+#define UDPPacket_H_
+// -----------------------------------------------------------------------------
+#include <list>
+#include <limits> 
+#include <ostream>
+#include "UniSetTypes.h"
+// -----------------------------------------------------------------------------
+namespace UniSetUDP
+{
+	struct UDPHeader
+	{
+		UDPHeader():num(0),nodeID(0),procID(0),dcount(0){}
+		unsigned long num;
+		long nodeID;
+		long procID;
+		size_t dcount;
+		
+		friend std::ostream& operator<<( std::ostream& os, UDPHeader& p );
+	}__attribute__((packed));
+
+	static unsigned long MaxPacketNum = std::numeric_limits<unsigned long>::max();
+	
+	struct UDPData
+	{
+		UDPData():id(UniSetTypes::DefaultObjectId),val(0){}
+		UDPData(long id, long val):id(id),val(val){}
+
+		long id;
+		long val;
+		
+		friend std::ostream& operator<<( std::ostream& os, UDPData& p );
+	}__attribute__((packed));
+
+	static const int MaxDataLen = 8192;
+	static const int MaxDataCount = ( MaxDataLen - sizeof(UniSetUDP::UDPHeader) ) / sizeof(UDPData);
+
+	 struct DataPacket
+	 {
+		UDPHeader header;
+		UDPData dat[MaxDataCount];
+	 }__attribute__((packed));
+
+
+	struct UDPMessage:
+		public UDPHeader
+	{
+		UDPMessage();
+
+		bool addData( const UDPData& dat );
+		bool addData( long id, long val );
+
+		inline bool isFull(){ return count<MaxDataCount; }
+		inline int size(){ return count; }
+		inline int byte_size(){ return count*sizeof(UDPData); }
+		
+		DataPacket msg;
+		int count;
+		
+		friend std::ostream& operator<<( std::ostream& os, UDPMessage& p );
+	};
+
+
+}
+// -----------------------------------------------------------------------------
+#endif // UDPPacket_H_
+// -----------------------------------------------------------------------------
