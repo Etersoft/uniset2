a89f8c8654e702551dcbe148b67c7deed87a59bf
Pavel Vaynerman <pv@etersoft.ru>
2011-12-21 21:15:47 +0400

    (LogicProcessor):  исправил баг с определением iotype датчика

--- extensions/LogicProcessor/TNOT.cc
+++ extensions/LogicProcessor/TNOT.cc
@@ -1,9 +1,10 @@
 #include <sstream>
 #include <iostream>
+#include "Extensions.h"
 #include "Element.h"
 // -------------------------------------------------------------------------
 using namespace std;
-
+using namespace UniSetExtensions;
 // -------------------------------------------------------------------------
 TNOT::TNOT( ElementID id, bool out_default ):
 	Element(id),
@@ -21,7 +22,9 @@ void TNOT::setIn( int num, bool state )
 	bool prev = myout;
 	myout = !state;
 	
-	cout << this << ": myout " << myout << endl;	
+	if( dlog.debugging(Debug::INFO) )
+		dlog[Debug::INFO] << this << ": myout " << myout << endl;	
+	
 	if( prev != myout )
 		Element::setChildOut();
 }
