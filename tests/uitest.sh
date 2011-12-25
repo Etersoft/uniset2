62a1ced47bc846b9fce6c23ad2198a06c0ff0c7e
Pavel Vaynerman <pv@etersoft.ru>
2011-12-26 00:22:48 +0400

    немного поменял тест

--- tests/uitest.sh
+++ tests/uitest.sh
@@ -4,6 +4,6 @@ SID=$1
 
 [ -z "$SID" ] && SID=1
 
-uniset-start.sh -f ./ui --confile test.xml --sid $SID
+uniset-start.sh -f ./ui --confile test.xml --sid $SID -ORBtraceLevel 20
 #--unideb-add-levels system,info,level9
 
