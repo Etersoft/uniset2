c70772f920c9d22006c8f1c72500addae2bfe7ca
Pavel Vaynerman <pv@etersoft.ru>
2012-03-15 20:52:54 +0400

    (unet2): исправил ошибку в конфигурировании датчика связи

--- extensions/UNet2/start_fg.sh
+++ extensions/UNet2/start_fg.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 uniset-start.sh -f ./uniset-unetexchange --unet-name UNetExchange \
-	--confile test.xml \
+	--confile test.xml --smemory-id SharedMemory \
 	--unet-filter-field rs --unet-filter-value 2 --unet-maxdifferense 40 \
 	--dlog-add-levels info,crit,warn
+
