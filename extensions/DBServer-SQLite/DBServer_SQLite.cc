1e332ce8c10fe37e1228ea880158c8ae92160077
Pavel Vaynerman <pv@etersoft.ru>
2012-12-24 01:21:26 +0400

    (SQLite): заготовка для интерфейса..

---
+++ extensions/DBServer-SQLite/DBServer_SQLite.cc
@@ -0,0 +1,468 @@
+/* This file is part of the UniSet project
+ * Copyright (c) 2002 Free Software Foundation, Inc.
+ * Copyright (c) 2002 Pavel Vainerman
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+// --------------------------------------------------------------------------
+/*! \file
+ *  \brief файл реализации DB-сервера
+ *  \author Pavel Vainerman
+*/
+// --------------------------------------------------------------------------
+#include <sys/time.h>
+#include <sstream>
+#include <iomanip>
+
+#include "ORepHelpers.h"
+#include "DBServer_SQLite.h"
+#include "Configuration.h"
+#include "Debug.h"
+#include "UniXML.h"
+// --------------------------------------------------------------------------
+using namespace UniSetTypes;
+using namespace std;
+// --------------------------------------------------------------------------
+const Debug::type DBLEVEL = Debug::LEVEL1;
+// --------------------------------------------------------------------------
+DBServer_SQLite::DBServer_SQLite( ObjectId id ):
+	DBServer(id),
+	db(new SQLiteInterface()),
+	PingTime(300000),
+	ReconnectTime(180000),
+	connect_ok(false),
+	activate(true),
+	lastRemove(false)
+{
+	if( getId() == DefaultObjectId )
+	{
+		ostringstream msg;
+		msg << "(DBServer_SQLite): init failed! Unknown ID!" << endl;
+		throw Exception(msg.str());
+	}
+}
+
+DBServer_SQLite::DBServer_SQLite():
+	DBServer(conf->getDBServer()),
+	db(new SQLiteInterface()),
+	PingTime(300000),
+	ReconnectTime(180000),
+	connect_ok(false),
+	activate(true),
+	lastRemove(false)
+{
+//	init();
+	if( getId() == DefaultObjectId )
+	{
+		ostringstream msg;
+		msg << "(DBServer_SQLite): init failed! Unknown ID!" << endl;
+		throw Exception(msg.str());
+	}
+}
+//--------------------------------------------------------------------------------------------
+DBServer_SQLite::~DBServer_SQLite()
+{
+	if( db != NULL )
+	{
+		db->close();
+		delete db;
+	}
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::processingMessage( UniSetTypes::VoidMessage *msg )
+{
+	switch(msg->type)
+	{
+		case Message::Timer:
+		{
+			TimerMessage tm(msg);
+			timerInfo(&tm);
+			break;
+		}
+
+		default:
+			DBServer::processingMessage(msg);
+			break;
+	}
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::sysCommand( UniSetTypes::SystemMessage *sm )
+{
+	switch( sm->command )
+	{
+		case SystemMessage::StartUp:
+			break;
+
+		case SystemMessage::Finish:
+		{
+			activate = false;
+			db->close();
+		}
+		break;
+
+		case SystemMessage::FoldUp:
+		{
+			activate = false;
+			db->close();
+		}
+		break;
+
+		default:
+			break;
+	}
+}
+
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::parse( UniSetTypes::DBMessage* dbm )
+{
+	if( dbm->tblid == UniSetTypes::Message::Unused )
+	{
+		unideb[Debug::CRIT] << myname <<  "(dbmessage): не задан tblId...\n";
+		return;
+	}
+
+	ostringstream query;
+	switch( dbm->qtype )
+	{
+		case DBMessage::Query:
+			query << dbm->data;
+			break;
+
+		case DBMessage::Update:
+			query << "UPDATE " << tblName(dbm->tblid) << " SET " << dbm->data;
+			break;
+
+		case DBMessage::Insert:
+			query << "INSERT INTO " << tblName(dbm->tblid) << " VALUES (" << dbm->data << ")";
+			break;
+
+	}
+
+	if( !writeToBase(query.str()) )
+	{
+		unideb[Debug::CRIT] << myname <<  "(update): error: "<< db->error() << endl;
+//		if( dbm->qtype == DBMessage::Query )
+//			db->freeResult();
+	}
+
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::parse( UniSetTypes::ConfirmMessage* cem )
+{
+	try
+	{
+		ostringstream data;
+
+		data << "UPDATE " << tblName(cem->type)
+			<< " SET confirm='" << cem->confirm << "'"
+			<< " WHERE sensor_id='" << cem->sensor_id << "'"
+			<< " AND date='" << ui.dateToString(cem->time, "-")<<" '"
+			<< " AND time='" << ui.timeToString(cem->time, ":") <<" '"
+			<< " AND time_usec='" << cem->time_usec <<" '";
+
+		if( unideb.debugging(DBLEVEL) )
+			unideb[DBLEVEL] << myname << "(update_confirm): " << data.str() << endl;
+
+		if( !writeToBase(data.str()) )
+		{
+			if( unideb.debugging(Debug::CRIT) )
+				unideb[Debug::CRIT] << myname << "(update_confirm):  db error: "<< db->error() << endl;
+		}
+	}
+	catch( Exception& ex )
+	{
+		if( unideb.debugging(Debug::CRIT) )
+			unideb[Debug::CRIT] << myname << "(update_confirm): " << ex << endl;
+	}
+	catch( ... )
+	{
+		if( unideb.debugging(Debug::CRIT) )
+			unideb[Debug::CRIT] << myname << "(update_confirm):  catch..." << endl;
+	}
+}
+//--------------------------------------------------------------------------------------------
+bool DBServer_SQLite::writeToBase( const string& query )
+{
+	if( unideb.debugging(DBLogInfoLevel) )
+		unideb[DBLogInfoLevel] << myname << "(writeToBase): " << query << endl;
+//	cout << "DBServer_SQLite: " << query << endl;
+	if( !db || !connect_ok )
+	{
+		uniset_mutex_lock l(mqbuf,200);
+		qbuf.push(query);
+		if( qbuf.size() > qbufSize )
+		{
+			std::string qlost;
+			if( lastRemove )
+				qlost = qbuf.back();
+			else
+				qlost = qbuf.front();
+
+			qbuf.pop();
+
+			if( unideb.debugging(Debug::CRIT) )
+				unideb[Debug::CRIT] << myname << "(writeToBase): DB not connected! buffer(" << qbufSize
+						<< ") overflow! lost query: " << qlost << endl;
+		}
+		return false;
+	}
+
+	// На всякий скидываем очередь
+	flushBuffer();
+
+	// А теперь собственно запрос..
+	db->query(query);
+
+	// Дело в том что на INSERT И UPDATE запросы
+	// db->query() может возвращать false и надо самому
+	// отдельно проверять действительно ли произошла ошибка
+	// см. SQLiteInterface::query.
+	string err(db->error());
+	if( err.empty() )
+		return true;
+
+	return false;
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::flushBuffer()
+{
+	uniset_mutex_lock l(mqbuf,400);
+
+	// Сперва пробуем очистить всё что накопилось в очереди до этого...
+	while( !qbuf.empty() )
+	{
+		db->query( qbuf.front() );
+
+		// Дело в том что на INSERT И UPDATE запросы
+		// db->query() может возвращать false и надо самому
+		// отдельно проверять действительно ли произошла ошибка
+		// см. SQLiteInterface::query.
+		string err(db->error());
+
+		if( err.empty() && unideb.debugging(Debug::CRIT) )
+		{
+			unideb[Debug::CRIT] << myname << "(writeToBase): error: " << err <<
+				" lost query: " << qbuf.front() << endl;
+		}
+
+		qbuf.pop();
+	}
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::parse( UniSetTypes::SensorMessage *si )
+{
+	try
+	{
+		// если время не было выставлено (указываем время сохранения в БД)
+		if( !si->tm.tv_sec )
+		{
+			struct timezone tz;
+			gettimeofday(&si->tm,&tz);
+		}
+
+		// см. DBTABLE AnalogSensors, DigitalSensors
+		ostringstream data;
+		data << "INSERT INTO " << tblName(si->type)
+			<< "(date, time, time_usec, sensor_id, value, node) VALUES( '"
+											// Поля таблицы
+			<< ui.dateToString(si->sm_tv_sec,"-") << "','"	//  date
+			<< ui.timeToString(si->sm_tv_sec,":") << "','"	//  time
+			<< si->sm_tv_usec << "',"				//  time_usec
+			<< si->id << ","					//  sensor_id
+			<< si->value << ","				//  value
+			<< si->node << ")";				//  node
+
+		if( unideb.debugging(DBLEVEL) )
+			unideb[DBLEVEL] << myname << "(insert_main_history): " << data.str() << endl;
+
+		if( !writeToBase(data.str()) )
+		{
+			if( unideb.debugging(Debug::CRIT) )
+				unideb[Debug::CRIT] << myname <<  "(insert) sensor msg error: "<< db->error() << endl;
+		}
+	}
+	catch( Exception& ex )
+	{
+		unideb[Debug::CRIT] << myname << "(insert_main_history): " << ex << endl;
+	}
+	catch( ... )
+	{
+		unideb[Debug::CRIT] << myname << "(insert_main_history): catch ..." << endl;
+	}
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::init_dbserver()
+{
+	DBServer::init_dbserver();
+	if( unideb.debugging(DBLogInfoLevel) )
+		unideb[DBLogInfoLevel] << myname << "(init): ..." << endl;
+
+	if( connect_ok )
+	{
+		initDBTableMap(tblMap);
+		initDB(db);
+		return;
+	}
+
+	if( conf->getDBServer() == UniSetTypes::DefaultObjectId )
+	{
+		ostringstream msg;
+		msg << myname << "(init): DBServer OFF for this node.."
+			<< " In " << conf->getConfFileName()
+			<< " for this node dbserver=''";
+		throw NameNotFound(msg.str());
+	}
+
+	xmlNode* node = conf->getNode("LocalDBServer");
+	if( !node )
+		throw NameNotFound(string(myname+"(init): section <LocalDBServer> not found.."));
+
+	UniXML::iterator it(node);
+
+	unideb[DBLogInfoLevel] << myname << "(init): init connection.." << endl;
+	string dbname(conf->getProp(node,"dbname"));
+	string dbnode(conf->getProp(node,"dbnode"));
+	string user(conf->getProp(node,"dbuser"));
+	string password(conf->getProp(node,"dbpass"));
+
+	tblMap[UniSetTypes::Message::SensorInfo] = "main_history";
+	tblMap[UniSetTypes::Message::Confirm] = "main_history";
+
+	PingTime = conf->getIntProp(node,"pingTime");
+	ReconnectTime = conf->getIntProp(node,"reconnectTime");
+	qbufSize = conf->getArgPInt("--dbserver-buffer-size",it.getProp("bufferSize"),200);
+
+	if( findArgParam("--dbserver-buffer-last-remove",conf->getArgc(),conf->getArgv()) != -1 )
+		lastRemove = true;
+	else if( it.getIntProp("bufferLastRemove" ) !=0 )
+		lastRemove = true;
+	else
+		lastRemove = false;
+
+	if( dbnode.empty() )
+		dbnode = "localhost";
+
+	if( unideb.debugging(DBLogInfoLevel) )
+		unideb[DBLogInfoLevel] << myname << "(init): connect dbnode=" << dbnode
+		<< "\tdbname=" << dbname
+		<< " pingTime=" << PingTime
+		<< " ReconnectTime=" << ReconnectTime << endl;
+
+#if 0
+	if( !db->connect(dbnode, user, password, dbname) )
+	{
+//		ostringstream err;
+		if( unideb.debugging(Debug::CRIT) )
+			unideb[Debug::CRIT] << myname
+			<< "(init): DB connection error: "
+			<< db->error() << endl;
+//		throw Exception( string(myname+"(init): не смогли создать соединение с БД "+db->error()) );
+		askTimer(DBServer_SQLite::ReconnectTimer,ReconnectTime);
+	}
+	else
+	{
+		if( unideb.debugging(DBLogInfoLevel) )
+			unideb[DBLogInfoLevel] << myname << "(init): connect [OK]" << endl;
+		connect_ok = true;
+		askTimer(DBServer_SQLite::ReconnectTimer,0);
+		askTimer(DBServer_SQLite::PingTimer,PingTime);
+//		createTables(db);
+		initDB(db);
+		initDBTableMap(tblMap);
+		flushBuffer();
+	}
+#endif
+
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::createTables( SQLiteInterface *db )
+{
+	UniXML_iterator it( conf->getNode("Tables") );
+	if(!it)
+	{
+		if( unideb.debugging(Debug::CRIT) )
+			unideb[Debug::CRIT] << myname << ": section <Tables> not found.."<< endl;
+		throw Exception();
+	}
+
+	for( it.goChildren();it;it.goNext() )
+	{
+		if( it.getName() != "comment" )
+		{
+			if( unideb.debugging(DBLogInfoLevel) )
+				unideb[DBLogInfoLevel] << myname  << "(createTables): create " << it.getName() << endl;
+			ostringstream query;
+			query << "CREATE TABLE " << conf->getProp(it,"name") << "(" << conf->getProp(it,"create") << ")";
+			if( !db->query(query.str()) && unideb.debugging(Debug::CRIT) )
+				unideb[Debug::CRIT] << myname << "(createTables): error: \t\t" << db->error() << endl;
+		}
+	}
+}
+//--------------------------------------------------------------------------------------------
+void DBServer_SQLite::timerInfo( UniSetTypes::TimerMessage* tm )
+{
+	switch( tm->id )
+	{
+		case DBServer_SQLite::PingTimer:
+		{
+#if 0
+			if( !db->ping() )
+			{
+				if( unideb.debugging(Debug::WARN) )
+					unideb[Debug::WARN] << myname << "(timerInfo): DB lost connection.." << endl;
+				connect_ok = false;
+				askTimer(DBServer_SQLite::PingTimer,0);
+				askTimer(DBServer_SQLite::ReconnectTimer,ReconnectTime);
+			}
+			else
+			{
+				connect_ok = true;
+				if( unideb.debugging(DBLogInfoLevel) )
+					unideb[DBLogInfoLevel] << myname << "(timerInfo): DB ping ok" << endl;
+			}
+#endif
+		}
+		break;
+
+		case DBServer_SQLite::ReconnectTimer:
+		{
+			if( unideb.debugging(DBLogInfoLevel) )
+				unideb[DBLogInfoLevel] << myname << "(timerInfo): reconnect timer" << endl;
+			if( db->isConnection() )
+			{
+#if 0
+				if( db->ping() )
+				{
+					connect_ok = true;
+					askTimer(DBServer_SQLite::ReconnectTimer,0);
+					askTimer(DBServer_SQLite::PingTimer,PingTime);
+				}
+#endif
+				connect_ok = false;
+				if( unideb.debugging(Debug::WARN) )
+					unideb[Debug::WARN] << myname << "(timerInfo): DB no connection.." << endl;
+			}
+			else
+				init_dbserver();
+		}
+		break;
+
+		default:
+			if( unideb.debugging(Debug::WARN) )
+				unideb[Debug::WARN] << myname << "(timerInfo): Unknown TimerID=" << tm->id << endl;
+		break;
+	}
+}
+//--------------------------------------------------------------------------------------------
---
+++ extensions/DBServer-SQLite/SQLiteInterface.cc
@@ -0,0 +1,192 @@
+/* This file is part of the UniSet project
+ * Copyright (c) 2002 Free Software Foundation, Inc.
+ * Copyright (c) 2002 Pavel Vainerman
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+// --------------------------------------------------------------------------
+/*! \file
+ *  \author Pavel Vainerman
+*/
+// --------------------------------------------------------------------------
+#include <sstream>
+#include "UniSetTypes.h"
+#include "SQLiteInterface.h"
+// --------------------------------------------------------------------------
+using namespace std;
+using namespace UniSetTypes;
+// --------------------------------------------------------------------------
+
+SQLiteInterface::SQLiteInterface():
+db(0),
+lastQ(""),
+queryok(false),
+connected(false),
+opTimeout(300),
+opCheckPause(50)
+{
+}
+
+SQLiteInterface::~SQLiteInterface()
+{
+	close();
+	delete db;
+}
+
+// -----------------------------------------------------------------------------------------
+bool SQLiteInterface::connect( const string dbfile )
+{
+	int rc = sqlite3_open(dbfile.c_str(), &db);
+	if( !rc )
+	{
+		cerr << sqlite3_errmsg(db) << endl;
+		sqlite3_close(db);
+		db = 0;
+		connected = false;
+		return false;
+	}
+
+	if( rc==SQLITE_BUSY || rc==SQLITE_LOCKED || rc==SQLITE_INTERRUPT || rc==SQLITE_IOERR )
+	{
+		cerr << sqlite3_errmsg(db) << endl;
+		sqlite3_close(db);
+		db = 0;
+		connected = false;
+		return false;
+	}
+
+
+	connected = true;
+	return true;
+}
+// -----------------------------------------------------------------------------------------
+bool SQLiteInterface::close()
+{
+	if(db)
+		sqlite3_close(db);
+
+	return true;
+}
+// -----------------------------------------------------------------------------------------
+bool SQLiteInterface::insert( const string q )
+{
+	if( !db )
+		return false;
+
+	// char* errmsg;
+	sqlite3_stmt* pStmt;
+
+	// Компилируем SQL запрос
+	sqlite3_prepare(db, q.c_str(), -1, &pStmt, NULL);
+	int rc = sqlite3_step(pStmt);
+
+	if( rc==SQLITE_BUSY || rc==SQLITE_LOCKED || rc==SQLITE_INTERRUPT || rc==SQLITE_IOERR )
+	{
+		if( !wait(pStmt, SQLITE_DONE) )
+		{
+			sqlite3_finalize(pStmt);
+			queryok=false;
+			return false;
+		}
+	}
+
+	sqlite3_finalize(pStmt);
+	queryok=true;
+	return true;
+}
+// -----------------------------------------------------------------------------------------
+bool SQLiteInterface::query( const string q )
+{
+	if( !db )
+		return false;
+
+	// char* errmsg = 0;
+	sqlite3_stmt* pStmt;
+
+	// Компилируем SQL запрос
+	sqlite3_prepare(db, q.c_str(), -1, &pStmt, NULL);
+	int rc = sqlite3_step(pStmt);
+	if( rc==SQLITE_BUSY || rc==SQLITE_LOCKED || rc==SQLITE_INTERRUPT || rc==SQLITE_IOERR )
+	{
+		if( !wait(pStmt, SQLITE_ROW) )
+		{
+			sqlite3_finalize(pStmt);
+			queryok=false;
+			return false;
+		}
+	}
+
+	lastQ = q;
+
+//	int cnum = sqlite3_column_count(pStmt);
+
+/*
+	while( (rc = sqlite3_step(pStmt)) == SQLITE_ROW )
+	{
+		int coln = sqlite3_data_count(pStmt);
+		for( int j=0; j<coln; j++ )
+		{
+
+		}
+	}
+*/
+
+	sqlite3_finalize(pStmt);
+	queryok=true;
+	return true;
+}
+// -----------------------------------------------------------------------------------------
+bool SQLiteInterface::wait( sqlite3_stmt* stmt, int result )
+{
+	PassiveTimer ptTimeout(opTimeout);
+	while( !ptTimeout.checkTime() )
+	{
+		sqlite3_reset(stmt);
+		int rc = sqlite3_step(stmt);
+		if(  rc == result || rc == SQLITE_DONE )
+			return true;
+
+		msleep(opCheckPause);
+	}
+
+	return false;
+}
+// -----------------------------------------------------------------------------------------
+const string SQLiteInterface::error()
+{
+	if( !db )
+		return "";
+
+	return sqlite3_errmsg(db);
+}
+// -----------------------------------------------------------------------------------------
+const string SQLiteInterface::lastQuery()
+{
+	return lastQ;
+}
+// -----------------------------------------------------------------------------------------
+int SQLiteInterface::insert_id()
+{
+	if( !db )
+		return 0;
+
+	return sqlite3_last_insert_rowid(db);
+}
+// -----------------------------------------------------------------------------------------
+bool SQLiteInterface::isConnection()
+{
+	return connected;
+}
+// -----------------------------------------------------------------------------------------
