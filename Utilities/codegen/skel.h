e733bb7c8522a2d8ac0c6aa3bd8e3633d6bae03b
Pavel Vaynerman <pv@etersoft.ru>
2013-01-14 15:28:52 +0400

    (uniset-codegen): добавил генератор скелета класса (--make-skel)

---
+++ Utilities/codegen/skel.h
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------
+#ifndef Skel_H_
+#define Skel_H_
+// -----------------------------------------------------------------------------
+#include "Skel_SK.h"
+// -----------------------------------------------------------------------------
+class Skel:
+	public Skel_SK
+{
+	public:
+		Skel( UniSetTypes::ObjectId id, xmlNode* confnode = UniSetTypes::conf->getNode("Skel") );
+		virtual ~Skel();
+
+	protected:
+		Skel();
+
+		virtual void step();
+		virtual void sensorInfo( UniSetTypes::SensorMessage *sm );
+		virtual void timerInfo( UniSetTypes::TimerMessage *tm );
+		virtual void askSensors( UniversalIO::UIOCommand cmd );
+
+	private:
+};
+// -----------------------------------------------------------------------------
+#endif // Skel_H_
+// -----------------------------------------------------------------------------
