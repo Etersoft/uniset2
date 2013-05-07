30caa8eea9c874684981d776811c24308a291454
Pavel Vaynerman <pv@etersoft.ru>
2013-05-07 16:58:47 +0400

    (modbus): исправил ошибку в help (eterbug #9290)

--- Utilities/MBTester/mbslave.cc
+++ Utilities/MBTester/mbslave.cc
@@ -24,7 +24,7 @@ static void print_help()
 	printf("[-t|--timeout] msec  - Timeout. Default: 2000.\n");
 	printf("[-v|--verbose]       - Print all messages to stdout\n");
 	printf("[-d|--device] dev    - use device dev. Default: /dev/ttyS0\n");
-	printf("[-a|--myaddr] addr   - Modbus address for master. Default: 0x01.\n");
+	printf("[-a|--myaddr] addr   - Modbus address for this slave. Default: 0x01.\n");
 	printf("[-s|--speed] speed   - 9600,14400,19200,38400,57600,115200. Default: 38400.\n");
 	printf("[-v|--verbose]       - Print all messages to stdout\n");
 	printf("[-g|--f485]          - Use 485 Fastwel\n");
