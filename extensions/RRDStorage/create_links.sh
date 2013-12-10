56b35d9e636299cf37d9c551833f020cbde10939
Pavel Vaynerman <pv@etersoft.ru>
2013-12-10 15:53:15 +0400

    (RRDStorage): предварительная версия, реализовал создание БД и обновлений по таймеру.

---
+++ extensions/RRDStorage/create_links.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s -f ../../Utilities/scripts/uniset-start.sh
+ln -s -f ../../Utilities/scripts/uniset-stop.sh stop.sh
+ln -s -f ../../Utilities/scripts/uniset-functions.sh
+ln -s -f ../../conf/test.xml test.xml
---
+++ extensions/RRDStorage/start_fg.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+uniset-start.sh -f ./uniset-rrdstorage --confile test.xml \
+	--rrd-name RRDStorage1 \
+	--dlog-add-levels info,crit,warn
