e0cefc1f33a0379515f948e4f79bcdac24f91ccf
Pavel Vaynerman <pv@etersoft.ru>
2013-12-10 23:31:46 +0400

    (SQLite): добавил описание с примером использования

--- extensions/DBServer-SQLite/SQLiteInterface.h
+++ extensions/DBServer-SQLite/SQLiteInterface.h
@@ -33,6 +33,51 @@
 // ----------------------------------------------------------------------------
 class SQLiteResult;
 // ----------------------------------------------------------------------------
+/*!  \page SQLiteIntarface  Интерфейс к SQLite
+
+   Пример использования:
+\code
+	try
+	{
+		SQLiteInterface db;
+		if( !db.connect("test.db") )
+		{
+			cerr << "db connect error: " << db.error() << endl;
+			return 1;
+		}
+		
+		stringstream q;
+		q << "SELECT * from main_history";
+		
+		SQLiteResult r = db.query(q.str());
+		if( !r )
+		{
+			cerr << "db connect error: " << db.error() << endl;
+			return 1;
+		}
+
+		for( SQLiteResult::iterator it=r.begin(); it!=r.end(); it++ )
+		{
+			cout << "ROW: ";
+			SQLiteResult::COL col(*it);
+			for( SQLiteResult::COL::iterator cit = it->begin(); cit!=it->end(); cit++ )
+				cout << as_string(cit) << "(" << as_double(cit) << ")  |  ";
+			cout << endl;
+		}
+
+		db.close();
+	}
+	catch(Exception& ex)
+	{
+		cerr << "(test): " << ex << endl;
+	}
+	catch(...)
+	{
+		cerr << "(test): catch ..." << endl;
+	}
+\endcode
+*/
+// ----------------------------------------------------------------------------
 // Памятка:
 // Включение режима для журнала - "вести в памяти" (чтобы поберечь CompactFlash)
 // PRAGMA journal_mode = MEMORY
