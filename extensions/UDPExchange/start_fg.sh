4a0791b7f8ef7c12a98457f9f3e6f870aa95fe63
Pavel Vaynerman <pv@etersoft.ru>
2014-01-31 20:54:31 +0400

    Удалил лишние файлы после синхронизации (rebase + pathes) c uniset/2.0

--- extensions/UDPExchange/start_fg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-uniset-start.sh -f ./uniset-udpexchange --udp-name UDPExchange --udp-host 192.168.1.255 \
-	--udp-broadcast 1 --udp-polltime 100 \
-	--confile test.xml \
-	--dlog-add-levels info,crit,warn
-#	--udp-filter-field udp --udp-filter-value 1 \
--- extensions/UDPExchange/start_fg2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-uniset-start.sh -f ./uniset-udpexchange --udp-name UDPExchange2 --udp-host localhost --udp-port 3001 \
-	--confile test.xml \
-	--udp-filter-field udp --udp-filter-value 2we \
-	--udp-ip \
-	--dlog-add-levels info,crit,warn
-
