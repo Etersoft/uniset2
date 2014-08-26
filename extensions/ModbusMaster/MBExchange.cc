140a2f87cfd938931aff08dc2dac8a9c552722cd
Pavel Vaynerman <pv@etersoft.ru>
2014-08-27 01:02:54 +0400

    (Modbus): сделал более "жёсткую" проверку на наличие связи с устройством. Чтобы "связь была", устройство должно "отвечать без ошибок".

--- extensions/ModbusMaster/MBExchange.cc
+++ extensions/ModbusMaster/MBExchange.cc
@@ -2772,8 +2772,7 @@ void MBExchange::poll()
                 if( ex.err == ModbusRTU::erTimeOut && !d->ask_every_reg )
                     break;
 
-                // если контроллер хоть что-то ответил, то вроде как связь есть..
-                if( ex.err !=  ModbusRTU::erTimeOut )
+                if( ex.err == ModbusRTU::erNoError )
                     d->resp_real = true;
             }
 
