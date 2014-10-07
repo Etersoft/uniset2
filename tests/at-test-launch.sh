7bda4a432799eea26828f6ef79add80f0d421da5
Pavel Vaynerman <pv@etersoft.ru>
2014-10-07 15:12:20 +0400

    (tests): предварительная реализация с использованием automake testsuite

---
+++ tests/at-test-launch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+[ -n "$3" ] && exit $3
+
+cd $1
+exec $2
+exit 1
--- tests/tests_with_conf.sh
+++ tests/tests_with_conf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # '--' - нужен для отделения аоргументов catch, от наших..
-./tests_with_conf $* -- --confile tests_with_conf.xml --prop-id2 -10 --ulog-no-debug
+./tests_with_conf $* -- --confile tests_with_conf.xml --prop-id2 -10 --ulog-no-debug && exit 0 || exit 1
