68a8a5ffcfbc07cea5dc4a986657301417a709ca
Pavel Vaynerman <pv@etersoft.ru>
2014-10-20 13:15:59 +0400

    (tests): сделал при тесте "with SM" принудительное создание репозитория (вызов create)

--- extensions/tests/tests_with_sm.sh
+++ extensions/tests/tests_with_sm.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 # '--' - нужен для отделения аоргументов catch, от наших..
+cd ../../Utilities/Admin/
+uniset-start.sh -f ./create_links.sh
+uniset-start.sh -f ./create
+
+uniset-start.sh -f ./exist | grep -q UNISET_PLC/Controllers || exit 1
+cd -
+
 uniset-start.sh -f ./tests_with_sm $* -- --confile tests_with_sm.xml --e-startup-pause 10 --ulog-levels warn,crit --dlog-levels warn,crit 
