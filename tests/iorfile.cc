9233beea6a9625857dfcf99d348e663424d059d1
Pavel Vaynerman <pv@etersoft.ru>
2014-11-26 02:10:20 +0300

    Добавил небольшой тест для IORFile.

---
+++ tests/iorfile.cc
@@ -0,0 +1,28 @@
+#include <catch.hpp>
+
+#include <sstream>
+#include "Configuration.h"
+#include "UniSetTypes.h"
+#include "IORFile.h"
+using namespace std;
+using namespace UniSetTypes;
+
+TEST_CASE("IORFile", "[iorfile]" ) 
+{
+	CHECK( uniset_conf()!=nullptr );
+
+	ObjectId testID = 1;
+	const std::string iorstr("testIORstring");
+	IORFile ior;
+	ior.setIOR(testID,iorstr);
+	REQUIRE( ior.getIOR(testID) == iorstr );
+
+	// здесь выносим "наружу" немного "внутренней кухни" IORFile,
+	// т.к. снаружи не известно как формируется "название файла"
+	ostringstream s;
+	s << uniset_conf()->getLockDir() << testID;
+	CHECK( file_exist(s.str()) );
+
+	ior.unlinkIOR(testID);
+	CHECK_FALSE( file_exist(s.str()) );
+}
