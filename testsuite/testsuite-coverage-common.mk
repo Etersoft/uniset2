6d776e7940f17d6aaec6113e56eb20df129d9f99
Pavel Vaynerman <pv@etersoft.ru>
2014-12-19 01:15:13 +0300

    Корректировка coverage (добавил удаление временных файлов).

--- testsuite/testsuite-coverage-common.mk
+++ testsuite/testsuite-coverage-common.mk
@@ -6,6 +6,7 @@ cov: all
 	make check
 	$(LCOV) -c -d $(abs_srcdir) -o $(abs_srcdir)/.coverage.run
 	$(LCOV) -d $(abs_srcdir) -a $(abs_srcdir)/.coverage.base -a $(abs_srcdir)/.coverage.run -o $(abs_srcdir)/.coverage.total
-	$(LCOV) -r $(abs_srcdir)/.coverage.total '/usr/include/c++/*' '/usr/include/cc++/*' '/usr/include/omniORB4/*' '/usr/include/sigc++-2.0/sigc++/*' -d $(abs_srcdir) -o $(abs_srcdir)/.coverage.total
+	$(LCOV) -r $(abs_srcdir)/.coverage.total '/usr/include/c++/*' '/usr/include/cc++/*' '/usr/include/omniORB4/*' '/usr/include/sigc++-2.0/sigc++/*'\
+	'/usr/include/catch.hpp' -d $(abs_srcdir) -o $(abs_srcdir)/.coverage.total
 	$(LCOV_GENHTML) --no-branch-coverage -o $(COVERAGE_REPORT_DIR) $(abs_srcdir)/.coverage.total
 	rm -f $(abs_srcdir)/.coverage.base $(abs_srcdir)/.coverage.run $(abs_srcdir)/.coverage.total
