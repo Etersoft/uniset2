432d5aecf782630e6ee9b4342b916f3885ec709a
Vitaly Lipatov <lav@etersoft.ru>
2015-10-24 18:15:54 +0300

    relax with cxxflags

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,6 @@
 
 autoreconf -fiv
 
-# set flags as in rpm build
-CFLAGS='-pipe -O2 -march=i586 -mtune=i686'
-export CFLAGS
-CXXFLAGS="$CFLAGS"
-export CXXFLAGS
+export CXXFLAGS='-pipe -O2 -pedantic -Wall -std=c++11'
 
 ./configure --enable-maintainer-mode --prefix=/usr $*
