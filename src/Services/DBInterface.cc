19bc28b1acad60911819ab1202f2b66d0505f45f
Vinogradov Aleksei <uzum@server>
2015-12-23 15:35:25 +0400

    Исправил ошибку в парсинге строки param

--- src/Services/DBInterface.cc
+++ src/Services/DBInterface.cc
@@ -34,7 +34,6 @@ bool DBNetInterface::connect( const std::string& param )
 		dbname = param.substr(prev, pos - prev);
 		break;
 	}
-
 	return nconnect( host, user, pswd, dbname );
 }
 //--------------------------------------------------------------------------------------------
