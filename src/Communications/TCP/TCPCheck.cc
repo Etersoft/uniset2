20b3449d4182fda416a8e495af04979261f0d62e
Pavel Vaynerman <pv@etersoft.ru>
2016-11-23 02:10:47 +0300

    (TCPCheck): переделал класс на использование condition

--- src/Communications/TCP/TCPCheck.cc
+++ src/Communications/TCP/TCPCheck.cc
@@ -18,7 +18,6 @@
 #include <sstream>
 #include <cstdlib>
 #include "UniSetTypes.h"
-#include "PassiveTimer.h"
 #include "ThreadCreator.h"
 #include "TCPCheck.h"
 #include "UTCPStream.h"
@@ -38,52 +37,14 @@ TCPCheck::~TCPCheck() noexcept
 
 }
 // -----------------------------------------------------------------------------
-bool TCPCheck::check( const std::string& _iaddr, timeout_t tout, timeout_t sleep_msec )
+bool TCPCheck::check( const std::string& _iaddr, timeout_t tout )
 {
 	auto v = uniset::explode_str(_iaddr, ':');
 
 	if( v.size() < 2 )
 		return false;
 
-	return check( v[0], uniset::uni_atoi(v[1]), tout, sleep_msec );
-}
-// -----------------------------------------------------------------------------
-bool TCPCheck::check( const std::string& _ip, int _port, timeout_t tout, timeout_t sleep_msec )
-{
-	ip = _ip;
-	port = _port;
-	tout_msec = tout;
-
-	setResult(false);
-
-	ThreadCreator<TCPCheck> t(this, &TCPCheck::check_thread);
-	//	t.setCancel(ost::Thread::cancelDeferred);
-	t.start();
-
-	PassiveTimer pt(tout);
-
-	while( !pt.checkTime() && t.isRunning() )
-		msleep(sleep_msec);
-
-	if( t.isRunning() ) // !getResult() )
-		t.stop();
-
-	return result;
-}
-// -----------------------------------------------------------------------------
-void TCPCheck::check_thread()
-{
-	setResult(false);
-
-	try
-	{
-		UTCPStream t;
-		t.create(ip, port, tout_msec);
-		t.setKeepAliveParams( (tout_msec > 1000 ? tout_msec / 1000 : 1) );
-		setResult(true);
-		t.disconnect();
-	}
-	catch( ... ) {}
+	return check( v[0], uniset::uni_atoi(v[1]), tout );
 }
 // -----------------------------------------------------------------------------
 template<typename T>
@@ -95,6 +56,7 @@ class TGuard
 			t(m, a)
 		{
 			t.start();
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 
 		~TGuard()
@@ -103,37 +65,71 @@ class TGuard
 				t.stop();
 		}
 
-		inline bool isRunning()
-		{
-			return t.isRunning();
-		}
-
 	protected:
 		ThreadCreator<T> t;
 };
-
 // -----------------------------------------------------------------------------
-bool TCPCheck::ping( const std::string& _ip, timeout_t tout, timeout_t sleep_msec, const std::string& _ping_args )
+bool TCPCheck::check( const std::string& _ip, int _port, timeout_t tout )
+{
+	ip = _ip;
+	port = _port;
+	tout_msec = tout;
+	thr_finished = false;
+	result = false;
+
+	TGuard<TCPCheck> t(this, &TCPCheck::check_thread);
+
+	std::unique_lock<std::mutex> lock(thr_mutex);
+	thr_event.wait_for(lock,std::chrono::milliseconds(tout), [=]()
+	{
+		return ( thr_finished == true );
+	} );
+
+	return result;
+}
+// -----------------------------------------------------------------------------
+void TCPCheck::check_thread()
+{
+	thr_finished = false;
+	result = false;
+
+	try
+	{
+		UTCPStream t;
+		t.create(ip, port, tout_msec);
+		t.setKeepAliveParams( (tout_msec > 1000 ? tout_msec / 1000 : 1) );
+		result = true;
+		t.disconnect();
+	}
+	catch( ... ) {}
+
+	thr_finished = true;
+}
+// -----------------------------------------------------------------------------
+bool TCPCheck::ping( const std::string& _ip, timeout_t tout, const std::string& _ping_args )
 {
 	ip = _ip;
 	tout_msec = tout;
 	ping_args = _ping_args;
+	thr_finished = false;
+	result = false;
 
-	setResult(false);
 
 	TGuard<TCPCheck> t(this, &TCPCheck::ping_thread);
 
-	PassiveTimer pt(tout);
-
-	while( !pt.checkTime() && t.isRunning() )
-		msleep(sleep_msec);
+	std::unique_lock<std::mutex> lock(thr_mutex);
+	thr_event.wait_for(lock,std::chrono::milliseconds(tout), [=]()
+	{
+		return ( thr_finished == true );
+	} );
 
 	return result;
 }
 // -----------------------------------------------------------------------------
 void TCPCheck::ping_thread()
 {
-	setResult(false);
+	thr_finished = false;
+	result = false;
 
 	ostringstream cmd;
 	cmd << "ping " << ping_args << " " << ip << " 2>/dev/null 1>/dev/null";
@@ -141,7 +137,8 @@ void TCPCheck::ping_thread()
 	int ret = system(cmd.str().c_str());
 	int res = WEXITSTATUS(ret);
 
-	setResult((res == 0));
+	result = (res == 0);
+	thr_finished = true;
 }
 // -----------------------------------------------------------------------------
 } // end of namespace uniset
