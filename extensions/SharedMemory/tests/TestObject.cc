3338255dc806e852bcb8d01c0e0d2bb309e48dcb
Pavel Vaynerman <pv@etersoft.ru>
2017-05-06 20:27:26 +0300

    (SM): добавил тест на проверку отсутствия потери сообщений SensorMessage и на отсутствие их перемешивания.

--- extensions/SharedMemory/tests/TestObject.cc
+++ extensions/SharedMemory/tests/TestObject.cc
@@ -36,6 +36,26 @@ void TestObject::sysCommand( const uniset::SystemMessage* sm )
 		evntIsOK = true;
 }
 // -----------------------------------------------------------------------------
+void TestObject::sensorInfo( const SensorMessage* sm )
+{
+	if( sm->id == monotonic_s )
+	{
+		if( (sm->value - lastValue) < 0 )
+			monitonicFailed = true;
+
+		if( (sm->value - lastValue) > 1 )
+		{
+			cerr << "LOST: sm->value=" << sm->value << " last=" << lastValue
+				 << " lost: " << (sm->value - lastValue)
+				 << endl;
+
+			lostMessages = true;
+		}
+
+		lastValue = sm->value;
+	}
+}
+// -----------------------------------------------------------------------------
 void TestObject::stopHeartbeat()
 {
 	maxHeartBeat = 0;
@@ -44,5 +64,42 @@ void TestObject::stopHeartbeat()
 void TestObject::runHeartbeat( int max )
 {
 	maxHeartBeat = max;
+}
+// -----------------------------------------------------------------------------
+void TestObject::askMonotonic()
+{
+	askSensor(monotonic_s, UniversalIO::UIONotify);
+}
+// -----------------------------------------------------------------------------
+void TestObject::startMonitonicTest()
+{
+	monitonicFailed = false;
+	lostMessages = false;
+	lastValue = in_monotonic_s;
+}
+// -----------------------------------------------------------------------------
+bool TestObject::isMonotonicTestOK() const
+{
+	return !monitonicFailed;
+}
+// -----------------------------------------------------------------------------
+bool TestObject::isLostMessages() const
+{
+	return lostMessages;
+}
+
+long TestObject::getLastValue() const
+{
+	return lastValue;
+}
+
+bool TestObject::isEmptyQueue()
+{
+	return ( countMessages() == 0 );
+}
+
+bool TestObject::isFullQueue()
+{
+	return (getCountOfLostMessages() > 0);
 }
 // -----------------------------------------------------------------------------
