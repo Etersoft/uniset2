9fd0ec778d9dfc318fbfbd6cd925483c0197ecbc
Pavel Vaynerman <pv@etersoft.ru>
2017-06-01 01:53:34 +0300

    (MBSlave): добавил новый тест (запуск до 200 MBSlave)

---
+++ extensions/ModbusSlave/tests/mbslave_perf_test.cc
@@ -0,0 +1,72 @@
+// --------------------------------------------------------------------------
+#include <string>
+#include "UniSetActivator.h"
+#include "SharedMemory.h"
+#include "MBSlave.h"
+// --------------------------------------------------------------------------
+using namespace std;
+using namespace uniset;
+// --------------------------------------------------------------------------
+int main( int argc, const char** argv )
+{
+	try
+	{
+		auto conf = uniset_init(argc, argv);
+
+		auto act = UniSetActivator::Instance();
+
+		// ------------ SharedMemory ----------------
+		auto shm = SharedMemory::init_smemory(argc, argv);
+
+		if( !shm )
+			return 1;
+
+		act->add(shm);
+
+		int num = conf->getArgPInt("--numproc", 10);
+
+		for( int i = 1; i <= num; i++)
+		{
+//			auto mbs = MBSlave::init_mbslave(argc, argv, shm->getId(), shm, "mbs");
+			ostringstream s;
+			s << "MBTCP" << i;
+
+			cout << "..create " << s.str() << endl;
+
+			ostringstream p;
+			p << "mbs" << i;
+
+			auto mbs = make_shared<MBSlave>( conf->getObjectID(s.str()),shm->getId(), shm, p.str());
+
+			if( !mbs )
+			{
+				cerr << "Ошибка при создании MBSlave " << s.str() << endl;
+				return 1;
+			}
+
+			act->add(mbs);
+		}
+
+		// -------------------------------------------
+		SystemMessage sm(SystemMessage::StartUp);
+		act->broadcast( sm.transport_msg() );
+
+		act->run(false);
+		return 0;
+	}
+	catch(Exception& ex)
+	{
+		cerr << "(mbslave_perf_test): " << ex << endl;
+	}
+	catch( CORBA::SystemException& ex )
+	{
+		cerr << "(mbslave_perf_test): " << ex.NP_minorString() << endl;
+	}
+	catch(...)
+	{
+		cerr << "(mbslave_perf_test): catch(...)" << endl;
+	}
+
+	return 1;
+}
+// --------------------------------------------------------------------------
---
+++ extensions/ModbusSlave/tests/mbslave_perf_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# '--' - нужен для отделения аргументов catch, от наших..
+cd ../../../Utilities/Admin/
+./uniset2-start.sh -f ./create_links.sh
+./uniset2-start.sh -f ./create
+
+./uniset2-start.sh -f ./exist | grep -q UNISET_PLC/Controllers || exit 1
+cd -
+
+
+MAX=200
+
+PARAMS=
+for n in `seq 1 $MAX`; do
+   PARAMS="${PARAMS} --mbs${n}-name MBTCP${n} --mbs${n}-inet-port ${n+80000} --mbs${n}-inet-addr 127.0.0.1 \
+   --mbs${n}-type TCP --mbs${n}-filter-field mbtcp --mbs${n}-filter-value 2 --mbs${n}-confnode MBSlave2 \
+   --mbs${n}-log-add-levels crit,warn"
+done
+
+./uniset2-start.sh -f ./mbslave-perf-test --confile test.xml ${PARAMS} --numproc $MAX $*
