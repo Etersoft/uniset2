809d62c71de088b70384d9c53b80885e60587255
Pavel Vaynerman <pv@etersoft.ru>
2017-06-03 03:39:54 +0300

    (EventLoopServer): с promise/future какие-то проблеммы (возможно при совместной работе с libev). Вернулся на использование std::condition_variable

--- src/Processes/EventLoopServer.cc
+++ src/Processes/EventLoopServer.cc
@@ -11,6 +11,9 @@ namespace uniset
 	{
 		evterm.set(loop);
 		evterm.set<EventLoopServer, &EventLoopServer::onStop>(this);
+
+		evruntimer.set(loop);
+		evruntimer.set<EventLoopServer, &EventLoopServer::onLoopOK>(this);
 	}
 	// -------------------------------------------------------------------------
 	EventLoopServer::~EventLoopServer()
@@ -19,26 +22,24 @@ namespace uniset
 			evstop();
 	}
 	// ---------------------------------------------------------------------------
-	bool EventLoopServer::evrun( bool thread )
+	bool EventLoopServer::evrun( bool thread, size_t timeout_msec )
 	{
 		if( isrunning )
 			return true;
 
 		isrunning = true;
 
-		std::promise<bool> p;
-
-		auto prepareOK = p.get_future();
-
 		if( !thread )
 		{
-			defaultLoop(p);
-			return prepareOK.get();
+			defaultLoop(); // <-- здесь бесконечный цикл..
+			return false;
 		}
-		else if( !thr )
-			thr = make_shared<std::thread>( [ &p, this ] { defaultLoop(std::ref(p)); } );
 
-		bool ret = prepareOK.get();
+		if( !thr )
+			thr = make_shared<std::thread>( [&] { defaultLoop(); } );
+
+		bool ret = waitDefaultLoopRunning(timeout_msec);
+
 		// если запуститься не удалось
 		if( !ret && thr )
 		{
@@ -81,13 +82,17 @@ namespace uniset
 		loop.break_loop(ev::ALL);
 	}
 	// -------------------------------------------------------------------------
-	void EventLoopServer::defaultLoop( std::promise<bool>& prepareOK ) noexcept
+	void EventLoopServer::defaultLoop() noexcept
 	{
 		evterm.start();
+
+		// нам нужен "одноразовый таймер"
+		// т.к. нам надо просто зафиксировать, что loop начал работать
+		evruntimer.start(0);
+
 		try
 		{
 			evprepare();
-			prepareOK.set_value(true);
 
 			while( !cancelled )
 			{
@@ -104,10 +109,33 @@ namespace uniset
 		catch( std::exception& ex )
 		{
 			cerr << "(EventLoopServer::defaultLoop): " << ex.what() << endl;
-			prepareOK.set_value(false);
 		}
 
+		looprunOK_state = false;
+		looprunOK_event.notify_all();
+
 		isrunning = false;
+	}
+	// -------------------------------------------------------------------------
+	bool EventLoopServer::waitDefaultLoopRunning( size_t waitTimeout_msec )
+	{
+		std::unique_lock<std::mutex> lock(looprunOK_mutex);
+		looprunOK_event.wait_for(lock, std::chrono::milliseconds(waitTimeout_msec), [&]()
+		{
+			return (looprunOK_state == true);
+		} );
+
+		return looprunOK_state;
+	}
+	// -------------------------------------------------------------------------
+	void EventLoopServer::onLoopOK( ev::timer& t, int revents ) noexcept
+	{
+		if( EV_ERROR & revents )
+			return;
+
+		looprunOK_state = true;
+		looprunOK_event.notify_all();
+		t.stop();
 	}
 	// -------------------------------------------------------------------------
 } // end of uniset namespace
