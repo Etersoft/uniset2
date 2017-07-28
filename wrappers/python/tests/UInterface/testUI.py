97c81e5242e16ce1c16bf6747b2bb1000515df96
Pavel Vaynerman <pv@etersoft.ru>
2017-07-28 21:03:00 +0300

    Рефакторинг структуры каталогов: выделил Core-компоненты, в отдельный подкаталог "Core". Перенёс python --> wrappers/python

---
+++ wrappers/python/tests/UInterface/testUI.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+sys.path.append('../../')
+sys.path.append('../../lib/pyUniSet/')
+sys.path.append('../../lib/pyUniSet/.libs')
+
+from lib import *
+
+if __name__ == "__main__":
+
+	lst = Params_inst()
+
+	for i in range(0, len(sys.argv)):
+		if i >= Params.max:
+			break;
+
+		lst.add( sys.argv[i] )
+
+	prop = 'Test'
+	lst.add_str(prop)
+
+
+
+	try:
+		uniset_init_params( lst, "test.xml");
+
+		obj1 = UProxyObject("TestProc")
+		obj2 = UProxyObject("TestProc1")
+		uniset_activate_objects()
+
+
+		while True:
+			print "sleep..."
+			print "getValue: %d=%d" % ( 1, getValue(1) )
+			time.sleep(2)
+
+		print "getShortName: id=%d name=%s" % (1, getShortName(1))
+		print "     getName: id=%d name=%s" % (1, getName(1))
+		print " getTextName: id=%d name=%s" % (1, getTextName(1))
+		print "\n"
+		print "getShortName: id=%d name=%s" % (2, getShortName(2))
+		print "     getName: id=%d name=%s" % (2, getName(2))
+		print " getTextName: id=%d name=%s" % (2, getTextName(2))
+		print " getObjectID: id=%d name=%s" % (getObjectID("TestProc"),"TestProc")
+
+		try:
+			print "getValue: %d=%d" % ( 1, getValue(1) )
+		except UException, e:
+			print "getValue exception: " + str(e.getError())
+
+		try:
+			print "setValue: %d=%d" % (14,22)
+			setValue(14,22)
+		except UException, e:
+			print "setValue exception: " + str(e.getError())
+
+	except UException, e:
+		print "(testUI): catch exception: " + str(e.getError())
