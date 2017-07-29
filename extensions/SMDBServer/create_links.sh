fa40da10e41edcd32159875c4bb27b9b1052ec42
Pavel Vaynerman <pv@etersoft.ru>
2017-07-29 03:03:06 +0300

    Удалил DEPRECATED компоненты: SharedMemoryPlus и SMDBServer

--- extensions/SMDBServer/create_links.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ln -s -f ../../Utilities/scripts/uniset2-start.sh
-ln -s -f ../../Utilities/scripts/uniset2-stop.sh stop.sh
-ln -s -f ../../Utilities/scripts/uniset2-functions.sh
-ln -s -f ../../conf/test.xml test.xml
-
--- extensions/SMDBServer/start_fg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-uniset2-start.sh -f ./uniset2-smdbserver --confile test.xml \
-	--dbserver-name DBServer \
-	--dlog-add-levels info,crit,warn
-	
