f40c4b0cb147938838c95eeec95c194732242d1a
Pavel Vaynerman <pv@etersoft.ru>
2020-04-08 15:50:11 +0300

    added 'table example' for DBServer_PGSQL

---
+++ extensions/DBServer-PostgreSQL/db_create_example.sql
@@ -0,0 +1,11 @@
+CREATE TABLE main_history (
+  id PRIMARY KEY NOT NULL,
+  date date NOT NULL,
+  time time NOT NULL,
+  time_usec int NOT NULL CHECK (time_usec >= 0),
+  sensor_id int NOT NULL,
+  value double precision NOT NULL,
+  node int NOT NULL,
+  confirm int DEFAULT NULL,
+  PRIMARY KEY (id)
+);
