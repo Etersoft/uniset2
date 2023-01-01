a1decd3b9e27c0edda03dba8f01bd160945a6e13
Pavel Vaynerman <pv@etersoft.ru>
2023-01-01 18:55:46 +0300

    (opcuagate): initial commit (base on libopen62541)

---
+++ extensions/OPCUAGate/tests/tests_with_sm.cc
@@ -0,0 +1,60 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
+#include <string>
+#include "Debug.h"
+#include "UniSetActivator.h"
+#include "PassiveTimer.h"
+#include "SharedMemory.h"
+#include "Extensions.h"
+// --------------------------------------------------------------------------
+using namespace std;
+using namespace uniset;
+using namespace uniset::extensions;
+// --------------------------------------------------------------------------
+int main(int argc, const char* argv[] )
+{
+    try
+    {
+        Catch::Session session;
+
+        if( argc > 1 && ( strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0 ) )
+        {
+            cout << "--confile    - Использовать указанный конф. файл. По умолчанию configure.xml" << endl;
+            SharedMemory::help_print(argc, argv);
+            cout << endl << endl << "--------------- CATCH HELP --------------" << endl;
+            session.showHelp();
+            return 0;
+        }
+
+        int returnCode = session.applyCommandLine( argc, argv );
+        auto conf = uniset_init(argc, argv);
+
+        bool apart = findArgParam("--apart", argc, argv) != -1;
+
+        auto shm = SharedMemory::init_smemory(argc, argv);
+
+        if( !shm )
+            return 1;
+
+        return session.run();
+    }
+    catch( const SystemError& err )
+    {
+        cerr << "(tests_with_sm): " << err << endl;
+    }
+    catch( const uniset::Exception& ex )
+    {
+        cerr << "(tests_with_sm): " << ex << endl;
+    }
+    catch( const std::exception& e )
+    {
+        cerr << "(tests_with_sm): " << e.what() << endl;
+    }
+    catch(...)
+    {
+        cerr << "(tests_with_sm): catch(...)" << endl;
+    }
+
+    return 1;
+}
