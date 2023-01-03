81b300515cd14d3a96bbc5a85a268fafd1b5cf0e
Pavel Vaynerman <pv@etersoft.ru>
2023-01-03 17:15:18 +0300

    (opcua): supported "setup listen ip"

--- extensions/OPCUAGate/start_fg.sh
+++ extensions/OPCUAGate/start_fg.sh
@@ -3,6 +3,5 @@
 uniset2-start.sh -f ./uniset2-opcuagate --confile test.xml \
 	--opcua-name OPCUAGate1 --opcua-confnode OPCUAGate \
 	--opcua-s-filter-field opcua --opcua-s-filter-value 1 \
+	--opcua-log-add-levels any,-level6 \
 	$*
-
-#	--opcua-log-add-levels any \
