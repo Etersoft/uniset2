4b57cbf556828564b37a85b5bd9dfa80a155008f
Pavel Vaynerman <pv@etersoft.ru>
2023-01-04 16:04:31 +0300

    (opcua): added simple docs

--- extensions/OPCUAGate/OPCUAGate.h
+++ extensions/OPCUAGate/OPCUAGate.h
@@ -35,11 +35,74 @@ namespace uniset
 
       - \ref sec_OPCUAGate_Comm
       - \ref sec_OPCUAGate_Conf
+      - \ref sec_OPCUAGate_Sesnros_Conf
 
     \section sec_OPCUAGate_Comm Общее описание OPCUAGate
+    Данная реализация построена на использовании проекта https://github.com/open62541/open62541
+    и обёртки для с++ https://github.com/open62541pp/open62541pp
 
+    При старте запускается OPC UA сервер, реализующий возможность получать данные по протоколу OPC UA.
+    В отдельном потоке происходит обновление данных из SharedMemory (SM). По умолчанию все датчики доступны
+    для чтения, но при конфигурировании процесса можно указать датчики которые будут доступны на запись
+    через функции OPC UA. Процесс может работать напрямую с SM через указатель или как отдельно запускаемый
+    процесс работающий с SM через uniset RPC (remote call procedure).
 
     \section sec_OPCUAGate_Conf Настройка OPCUAGate
+    При старте, в конфигурационном файле ищется секция с названием объекта,
+    в которой указываются настроечные параметры по умолчанию.
+    Пример:
+    \code
+     <OPCUAGate name="OPCUAGate1" port="4840"
+         appName="uniset2 OPC UA gate"
+         shutdownDelay="5000"
+         maxSubscriptions="10"
+         maxSessions="10"
+         updatePause="200"
+    />
+    \endcode
+    К основным параметрам относятся
+     - \b updatePause - мсек, период с которым обновляются значения датчиков
+     - \b port - port на котором слушает OPC UA сервер
+
+    Остальные параметры определяют детали функционирования OPC UA сервера
+     https://www.open62541.org/doc/master/server.html#limits
+
+    Часть параметров берётся из настроек текущего локального узла в секции <nodes>
+    \code
+     <nodes ...>
+        <item id="3000" ip="127.0.0.1" name="node2" textname="Локальный узел"
+           opcua_ip="0.0.0.0"
+        />
+     ...
+     </nodes>
+     \endcode
+     В частности параметр \b opcua_ip - позволяет задать адрес на котором будет слушать OPC UA сервер.
+     По умолчанию адрес берётся из параметра \b ip.
+     В качестве \a browseName берётся \b name в качестве \a description - \b textname
+
+    См. так же help \a uniset2-opcuagate -h
+
+    \section sec_OPCUAGate_Sesnros_Conf Конфигурирование списка доступных переменных (variables) для OPC UA
+    Конфигурационные параметры задаются в секции <sensors> конфигурационного файла.
+    Список обрабатываемых регистров задаётся при помощи двух параметров командной строки
+     - \b --xxx-s-filter-field  - задаёт фильтрующее поле для датчиков
+     - \b --xxx-s-filter-value  - задаёт значение фильтрующего поля. Необязательный параметр.
+
+    Пример конфигурационных параметров для запуска с параметрами
+    `--opcua-s-filter-field opcua --opcua-s-filter-value 1`
+    \code
+    <sensors name="Sensors">
+     ...
+     <item name="MySensor_S" textname="my sesnsor" iotype="DI" opc_ua="1" opcua_rwmode="w"/>
+     <item name="OPCUA_Command_C" textname="opc ua command" iotype="DI" opc_ua="1" opcua_rwmode="w"/>
+     ...
+    </sensors>
+    \endcode
+     - \b opcua_rwmode - Режим доступа к датчику.
+       - \b w - датчик будет доступен на запись через OPC UA.
+       - \b none - не предоставлять доступ к датчику (не будет виден через OPC UA)
+
+     По умолчанию все датчики доступны только на чтение.
     */
     // -----------------------------------------------------------------------------
     /*! Реализация OPCUAGate */
