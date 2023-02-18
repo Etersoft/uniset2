20d112efaf44011c9478537c7df77bca09eba3ce
Pavel Vaynerman <pv@etersoft.ru>
2023-02-19 00:38:41 +0300

    cleanup deprecated files

--- .qtcreator/update-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find ../ -type f -name '*.cc' -or -name '*.cpp' -or -name '*.h' -or -name '*.src.xml' -or -name '*.am' | grep -v '_SK' | sort -n > uniset2.files
-echo ../conf/test.xml >> uniset2.files
