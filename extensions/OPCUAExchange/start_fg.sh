834763353fde0ccf4aa4cf736e0eb03594d51f6c
Pavel Vaynerman <pv@etersoft.ru>
2023-02-28 00:46:10 +0300

    (opcua-exchange): minor fixes

--- extensions/OPCUAExchange/start_fg.sh
+++ extensions/OPCUAExchange/start_fg.sh
@@ -3,7 +3,7 @@
 uniset2-start.sh -f ./uniset2-opcua-exchange --confile test.xml \
 	--smemory-id SharedMemory \
 	--opcua-name OPCUAClient1 --opcua-confnode OPCUAClient \
-	--opcua-s-filter-field opc --opcua-polltime 1000 \
-	--opcua-log-add-levels warn,crit,level5,level4,-level6,-level3,level7 \
-	--opcua-polltime 500 \
+	--opcua-s-filter-field opc \
+	--opcua-log-add-levels warn,crit,level5,level4,-level6,-level3,level7,level8 \
+	--opcua-polltime 50 --opcua-updatetime 50 \
 #	$*
